27d04aba271f422daa7f90135afe775ae5ca0223
phil <phil@pjd.me.uk>
2017-02-23 20:17:47 +0000

    Cleaned out everything for a rewrite

--- audio_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#sample pipelines to test the gstreamer plugins
-
-#gst-launch-1.0 playbin uri="file:///home/phil/Downloads/11 - Foxey Lady.mp3"
-
-gst-launch-1.0 filesrc location="Downloads/11 - Foxey Lady.mp3" ! decodebin ! audioconvert ! pitch tempo=1.5 ! autoaudiosink
